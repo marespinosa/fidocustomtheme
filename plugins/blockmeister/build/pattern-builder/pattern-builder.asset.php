5d55d183de1dc6e63a666162e367a4f2c7bae18a
marsespinosa <mariconespinosa888@gmail.com>
2021-12-23 13:36:03 +0800

    delete plugins

--- plugins/blockmeister/build/pattern-builder/pattern-builder.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editPost', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => '544e1f6e5c42020c8797e6fe61944174');
